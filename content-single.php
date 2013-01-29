290cddd03e894b179fa1803fab558affff9321e0
Jake Spurlock <whyisjake@gmail.com>
2013-01-29 14:00:33 -0800

    Updating the single post view.

--- content-single.php
+++ content-single.php
@@ -5,34 +5,37 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<div class="entry-meta">
-			<?php //mms_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>
+	<aside class="post-author">
+		<a href="<?php echo get_the_author_meta('user_url'); ?>">
+			<?php if('') : //currently no function exists for user photos. TO DO: BUILD ONE OR SOMETHING COOL LIKE THAT. ?>
+				<div class="author-photo">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dale-dougherty.jpg" />
+				</div>
+			<?php endif; ?>
+			<?php printf(__('by %1$s', 'mentor_makerspace'), get_the_author_meta('display_name')); ?>
+		</a>
+	</aside>
+	<section class="post-body">
+		<header class="entry-header">
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'mentor_makerspace' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'mentor_makerspace' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
+			<?php if('post' == get_post_type()) : ?>
+				<div class="entry-meta">
+					<?php mms_post_meta(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'mentor_makerspace' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', ', ' );
-
-			printf(
-				$category_list,
-				$tag_list,
-				get_permalink(),
-				the_title_attribute( 'echo=0' )
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit Post', 'mentor_makerspace' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
+		<?php if(is_search()) : // Only display Excerpts for Search ?>
+			<div class="entry-summary">
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-summary -->
+		<?php else : ?>
+			<div class="entry-content">
+				<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'mentor_makerspace')); ?>
+				<?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'mentor_makerspace'), 'after' => '</div>')); ?>
+			</div><!-- .entry-content -->
+		<?php endif; ?>
+	</section><!--[END .post-body]-->
 </article><!-- #post-<?php the_ID(); ?> -->
